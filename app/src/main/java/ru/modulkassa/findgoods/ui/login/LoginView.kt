bf030f9542d7e576c5f2bfdb5dd8370f019db31a
i.antashev <i.antashev@avanpos.com>
2018-03-15 23:23:12 +0700

    add comment

--- app/src/main/java/ru/modulkassa/findgoods/ui/login/LoginView.kt
+++ app/src/main/java/ru/modulkassa/findgoods/ui/login/LoginView.kt
@@ -3,6 +3,13 @@ package ru.modulkassa.findgoods.ui.login
 import com.arellomobile.mvp.MvpView
 
 interface LoginView : MvpView {
+    /**
+     * Показать ошибку
+     */
     fun showError()
+
+    /**
+     * Показать экран выбора точки
+     */
     fun showSelectScreen()
 }
